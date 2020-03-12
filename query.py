1e55a8cc6b8aaad41721505bcfd0880f702e05f2
Colby Tobin <tobincolby@gmail.com>
2020-03-11 22:01:42 -0400

    added yelp query

--- query.py
+++ query.py
@@ -5,17 +5,52 @@ import json
 import requests
 
 
+def searchQuery(location="NYC"):
+    searchQuery = '''
+        {
+            search(location: "%s") {
+                business {
+                    id
+                    name
+                    rating
+                    price
+                    display_phone
+                    url
+                    photos
+                    location {
+                        address1
+                        city
+                        state
+                        postal_code
+                        country
+                    }
+                }
+            }
+        }
+            ''' % (location)
+
+    return searchQuery
+
+def run_yelp_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+    headers = {"Authorization": "",
+    "Content-Type": "application/graphql",
+    }
+    request = requests.post('https://api.yelp.com/v3/graphql', data=query, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
 
 def query_flights(origin, destination, departureDate):
     origin += "-sky"
     destination += "-sky"
     url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/" + origin + "/"+destination+"/"+departureDate
 
-    headers = None
+    headers = {}
 
     response = requests.request("GET", url, headers=headers)
     text = json.loads(response.text)
-
     places = text["Places"]
     carriers = text["Carriers"]
     quotes = text["Quotes"]
@@ -47,5 +82,7 @@ def query_flights(origin, destination, departureDate):
 
 
     return return_info
+
+print(run_yelp_query(searchQuery()))
 
 print(query_flights("SFO", "ORD", "2020-05-05"))
